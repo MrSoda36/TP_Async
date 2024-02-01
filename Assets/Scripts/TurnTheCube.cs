d2ba16ddb1a2abee001a34c5b5e539929aac1e38
MrSoda36 <trystan.perello@outlook.fr>
2024-02-01 10:53:19 +0100

    Init Project + Exo 1 Done

---
+++ Assets/Scripts/TurnTheCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TurnTheCube : MonoBehaviour
+{
+    private void Start()
+    {
+        //StartCoroutine(TurnCube());
+    }
+
+    public void StartSpin()
+    {
+        StartCoroutine(TurnCube());
+    }
+
+    public IEnumerator TurnCube()
+    {
+        float passedTime = 0;
+        while (passedTime < 5f)
+        {
+            transform.Rotate(0, 45 * passedTime, 0);
+            passedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void StopCube()
+    {
+        StopAllCoroutines();
+    }
+}
